f3f5b4de0d209f7ea9cb20ddb06b28446d4d49ae
Michel <Michel@192.168.0.108>
2015-12-05 11:27:23 +0100

    // static Plugin plugin = null;

--- src/me/mickerd/mcacore/main.java
+++ src/me/mickerd/mcacore/main.java
@@ -1,17 +1,23 @@
 package me.mickerd.mcacore;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin {
 	
+	static Plugin plugin = null;
 	public void onEnable()
 	{
-		
+		plugin = this;
 	}
 	
 	public void onDisable()
 	{
-		
+		plugin = null;
 	}
 
+	public Plugin returnPL()
+	{
+		return plugin;
+	}
 }
